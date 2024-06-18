01947c8e3277513cb8b037e8debaef8c1542a402
Pittab <120305267+Pittab@users.noreply.github.com>
2024-06-18 07:43:35 +0100

    Added continuation query variable that automatically opens the load project function on load

--- script.js
+++ script.js
@@ -270,4 +270,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         quill.on('text-change', function(delta) {
         change = change.compose(delta);
     });
+    var toContinue=getQueryVariable("continue")
+    if (toContinue != "no"){
+        loadLocal()
+    }
 });
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  } 
+  return "no";
+}
